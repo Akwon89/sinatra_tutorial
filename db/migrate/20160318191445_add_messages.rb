a6fefae477eaa16da48fbaae662ef65a3fe6350a
Akwon89 <a.soonkwon@gmail.com>
2016-03-18 19:51:19 -0400

    remove_title

--- db/migrate/20160318191445_add_messages.rb
+++ db/migrate/20160318191445_add_messages.rb
@@ -1,7 +1,7 @@
 class AddMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :title
+      # t.string :title
       t.string :author
       t.string :content
     end
